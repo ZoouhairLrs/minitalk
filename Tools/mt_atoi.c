3d1ed5a151c7e1115af891c74faa94138237c9b3
Zouhair Laaroussi <92701395+ZoouhairLrs@users.noreply.github.com>
2023-01-03 00:17:00 +0100

    last commit tester

---
+++ Tools/mt_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mt_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 00:16:10 by zlaarous          #+#    #+#             */
+/*   Updated: 2023/01/03 00:16:35 by zlaarous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../includes/minitalk.h"
+
+int	my_atoi(char *str)
+{
+	size_t	i;
+	size_t	nbr;
+	int		symb;
+
+	symb = 1;
+	i = 0;
+	nbr = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		symb = str[i];
+		i++;
+	}
+	while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
+	{
+		nbr *= 10;
+		nbr += str[i] - '0';
+		i++;
+	}
+	return (check(nbr, symb));
+}
---
+++ Tools/mt_convert_to_decimal.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mt_convert_to_decimal.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 00:15:40 by zlaarous          #+#    #+#             */
+/*   Updated: 2023/01/03 00:15:49 by zlaarous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../includes/minitalk.h"
+
+int	mt_convert_to_decimal(int binary)
+{
+	int	decimal;
+	int	base;
+	int	rem;
+
+	decimal = 0;
+	base = 1;
+	while (binary != 0)
+	{
+		rem = binary % 10;
+		decimal = decimal + rem * base;
+		binary = binary / 10;
+		base = base * 2;
+	}
+	if (decimal > 127 || decimal < 0)
+	{
+		mt_putstr("\nError Recieving the signal\n");
+		exit(0);
+	}
+	else
+		return (decimal);
+}
